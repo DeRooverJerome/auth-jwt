5f2e1209ca43cd3d42fcda41923fdf6cd9c395d5
DeRooverJerome <derooverjdr@gmail.com>
2023-11-26 13:36:18 +0100

    final

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/User"); // Add this line
 
 const requireAuth = (req, res, next) => {
   const token = req.cookies.jwt;
@@ -18,3 +19,27 @@ const requireAuth = (req, res, next) => {
     res.redirect("/login");
   }
 };
+
+// check current user
+const checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, "net ninja secret", async (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        res.locals.user = null;
+        next();
+      } else {
+        console.log(decodedToken);
+        let user = await User.findById(decodedToken.id);
+        res.locals.user = user;
+        next();
+      }
+    });
+  } else {
+    res.locals.user = null;
+    next();
+  }
+};
+
+module.exports = { requireAuth, checkUser };
